aeb13fa9a99ed41094aeb3ff5002e0fb6bad1ed1
Novaturient <novaturiente@proton.me>
2025-05-01 11:58:00 +0530

    update

--- nova/.config/rofi/bookmarks.sh
+++ nova/.config/rofi/bookmarks.sh
@@ -1,43 +1,10 @@
 #!/bin/bash
-#
-# Github:
-# https://github.com/Strix007/rofi-bookmarks
-#
-# USAGE ############################################################################################################################################################
-#
-# - Add Bookmark
-#
-# To add a bookmark, start the line with + then your url. Default browser is settetd in your envirorment variables ($BROWSER)
-# Note: If your bookmark does not contain a domain extension, it will default to .com
-#
-# - Remove Bookmark
-#
-# To remove a bookmark, start the line with _ then your url
-# Note: It fuzzy matches the input to the saved bookmarks
-#
-# - Addition Usage
-#
-# If you type a address that is not saved in your saved bookmarks, it will open the browser with that address
-# Note: If the address does not contain a domain extension, it will default to a google search.
-#
-# - Saved Bookmarks
-#
-# The bookmarks are saved in a hidden plain text file in $HOME/.scripts/rofi/rofi-webookmarks/bookmarks
-# You can manually add bookmarks to $HOME/.scripts/rofi/rofi-webookmarks/bookmarks by starting a new line your bookmark. For Example - youtube.com
-# Note: Make sure every bookmark is on a seperate line
-#
-#################################################################################################################################################################
-#
-DIR=""
+
+# Configuration Variables
 THEME="bookmarks"
 BOOKMARKS_FILE="$HOME/.dotfiles/.bookmarks"
-# Note: the default browser is setted by the default envirorment variable $BROWSER
-# You can change in uncommenting the following line (the example browser is "firefox")
 BROWSER="surf"
-
 export GDK_BACKEND=x11
-
-# Check if there is a bookmarks file and if not, make one
 
 if [[ ! -a "${BOOKMARKS_FILE}" ]]; then
     touch "${BOOKMARKS_FILE}"
@@ -45,22 +12,26 @@ fi
 
 INPUT=$(rofi -dmenu -theme bookmarks.rasi -p "#B#" < "$BOOKMARKS_FILE")
 
-if   [[ $INPUT == "+"* ]]; then
-    INPUT=$(echo $INPUT | sed 's/+//')
-    if [[ $INPUT == *"."* ]]; then
-        echo "$INPUT" >> "$BOOKMARKS_FILE"
-    else 
-        INPUT="${INPUT}.com" && echo "$INPUT" >> "$BOOKMARKS_FILE"
-    fi
+if [[ $INPUT == "+"* ]]; then
+    INPUT="${INPUT#+}"
+    echo "$INPUT" >> "$BOOKMARKS_FILE"
+
 elif [[ $INPUT == "_"* ]]; then
-    INPUT=$(echo "$INPUT" | sed 's/_//') && sed -i "/$INPUT/d" "$BOOKMARKS_FILE"
-elif [[ $INPUT == *"."* ]]; then
+    INPUT="${INPUT#_}"
+    # Escape special characters for sed
+    ESCAPED_INPUT=$(printf '%s\n' "$INPUT" | sed 's/[][\\/.*^$]/\\&/g')
+    sed -i "/^$ESCAPED_INPUT$/d" "$BOOKMARKS_FILE"
+
+elif [[ $INPUT == *"."* || $INPUT == *"//"* ]]; then
+    # Escape special characters for sed
+    ESCAPED_INPUT=$(printf '%s\n' "$INPUT" | sed 's/[][\\/.*^$]/\\&/g')
+    sed -i "/^$ESCAPED_INPUT$/d" "$BOOKMARKS_FILE" # Remove original
+    sed -i "1i$INPUT" "$BOOKMARKS_FILE"            # Add to top
     $BROWSER "$INPUT"
-elif [[ -z $INPUT  ]]; then
+
+elif [[ -z $INPUT ]]; then
     exit 0
+
 else
     $BROWSER "https://www.google.com/search?q=$INPUT"
-    # $BROWSER --search $INPUT
-    # Set search engine (comment $BROWSER --search $INPUT and uncomment the following (example is duckducgo)
-    # $BROWSER "http://www.duckduckgo.com/search?q=$INPUT"
 fi
