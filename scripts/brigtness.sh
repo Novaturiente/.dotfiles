09823c9c3e83124e6644bae1e8c7938bd0f8b13d
Novaturiente <Novaturiente@proton.me>
2025-06-27 14:45:43 +0530

    update

--- scripts/brigtness.sh
+++ scripts/brigtness.sh
@@ -12,13 +12,13 @@ current_percent=$(brightnessctl get)
 # Determine adjustment step
 if [ "$1" = "up" ]; then
     if [ "$current_percent" -gt 1920 ]; then
-        swayosd-client --brightness +20
+        swayosd-client --brightness +5
     else
         swayosd-client --brightness +1
     fi
 elif [ "$1" = "down" ]; then
     if [ "$current_percent" -gt 1920 ]; then
-        swayosd-client --brightness -20
+        swayosd-client --brightness -5
     else
         swayosd-client --brightness -1
     fi
