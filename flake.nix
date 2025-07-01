54ca5104678ed928462aacbc2e36ccfeb4cbc534
Novaturiente <Novaturiente@proton.me>
2025-07-01 08:28:14 +0530

    update

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Nix profile";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";  # Pinned channel
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";  # Adjust for your CPU arch
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      # Define your package set
+      packages.${system} = {
+        myProfile = pkgs.buildEnv {
+          name = "extra-packages";
+          paths = with pkgs; [
+            opencode
+            tree
+          ];
+          extraOutputsToInstall = ["man" "doc"];
+        };
+      };
+    };
+}
