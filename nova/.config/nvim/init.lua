a17ef943c7a73d5e4cf712f84c684d934285cca0
Novaturiente <Novaturiente@proton.me>
2025-07-07 10:34:35 +0530

    update

--- nova/.config/nvim/init.lua
+++ nova/.config/nvim/init.lua
@@ -189,18 +189,14 @@ require('lazy').setup({
     config = true
   },
   {
-    "folke/flash.nvim",
+    "kylechui/nvim-surround",
+    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
-    ---@type Flash.Config
-    opts = {},
-    -- stylua: ignore
-    keys = {
-      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-    },
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
   },
   {
     'nvim-lualine/lualine.nvim',
@@ -803,5 +799,7 @@ wk.add{
   {"<leader>on", ":ObsidianNew<cr>", desc = "Obsidian new file"}
 }
 
--- File navigation
+-- Navigation
+vim.keymap.set({'n', 'v', 'i'}, '<C-h>', '^', { noremap = true })
+vim.keymap.set({'n', 'v', 'i'}, '<C-l>', '$', { noremap = true })
 
