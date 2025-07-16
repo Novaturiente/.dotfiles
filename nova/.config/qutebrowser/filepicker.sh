8d2c4fb18ee398e69e4c77493ebfa5a90f1015e1
Novaturiente <Novaturiente@proton.me>
2025-07-17 03:42:14 +0530

    Update

--- nova/.config/qutebrowser/filepicker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Ensure environment is sane
-export TERM=xterm-256color
-
-# Optional: sleep to let Ghostty settle (can fix render glitches)
-sleep 0.1
-
-# Run ranger with passed args
-exec ghostty -l --title="filepick" -e ranger "$@"
