327d0750be67a5d6f53e6e4d41da8827f7398378
Novaturiente <Novaturiente@proton.me>
2025-08-28 08:26:13 +0530

    Update

--- Packages/configure.sh
+++ Packages/configure.sh
@@ -4,3 +4,5 @@ sudo cp ../system/etc/modprobe.d/nvidia-power-management.conf /etc/modprobe.d/nv
 sudo cp ../system/etc/modules-load/ntsync.conf /etc/modules-load.d/ntsync.conf
 
 stow -t ~ ../nova
+
+sudo systemctl enable greetd
