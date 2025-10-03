1e7e1354b7dc5fda57fce64041b7c379ce6f3a66
Novaturiente <Novaturiente@proton.me>
2025-10-03 13:13:17 +0530

    updated bookmarks.sh

--- scripts/rofi/bookmarks.sh
+++ scripts/rofi/bookmarks.sh
@@ -4,7 +4,7 @@ BOOKMARKS_FILE="$HOME/.dotfiles/.bookmarks"
 BROWSER="qutebrowser"
 export GDK_BACKEND=wayland
 
-if [[ ! -a "${BOOKMARKS_FILE}" ]]; then
+if [[ ! -e "${BOOKMARKS_FILE}" ]]; then
     touch "${BOOKMARKS_FILE}"
 fi
 
@@ -15,13 +15,15 @@ if [ -n "$1" ]; then
         # Extract part between // and next /
         title=$(echo "$INPUT" | awk -F[/:] '{print $4}')
     fi
-    echo "${title}=${INPUT}" >> "$BOOKMARKS_FILE"
+    ENTRY="${title}=${INPUT}"
+    if ! grep -Fxq "$ENTRY" "$BOOKMARKS_FILE"; then
+        echo "$ENTRY" >>"$BOOKMARKS_FILE"
+    fi
 else
 
     LIST=$(cut -d'=' -f1 "$BOOKMARKS_FILE")
 
     INPUT=$(echo "$LIST" | rofi -dmenu -theme black -p "")
-
 
     if [[ $INPUT == "+"* ]]; then
         INPUT="${INPUT#+}"
@@ -30,11 +32,14 @@ else
             # Extract part between // and next /
             title=$(echo "$INPUT" | awk -F[/:] '{print $4}')
         fi
-        echo "${title}=${INPUT}" >> "$BOOKMARKS_FILE"
+        ENTRY="${title}=${INPUT}"
+        if ! grep -Fxq "$ENTRY" "$BOOKMARKS_FILE"; then
+            echo "$ENTRY" >>"$BOOKMARKS_FILE"
+        fi
 
     elif [[ $INPUT == "-" ]]; then
         # Show bookmark list for removal
-        SELECTED=$(rofi -dmenu -theme black -p "Remove bookmark" < "$BOOKMARKS_FILE")
+        SELECTED=$(rofi -dmenu -theme black -p "Remove bookmark" <"$BOOKMARKS_FILE")
         if [[ -n "$SELECTED" ]]; then
             sed -i "/^$(printf '%s' "$SELECTED" | sed 's/[]\/.^$*]/\\&/g')$/d" "$BOOKMARKS_FILE"
         fi
@@ -46,7 +51,7 @@ else
         # Escape special characters for sed
         LINK=$(grep "^${INPUT}=" "$BOOKMARKS_FILE" | cut -d'=' -f2-)
         LINE=$(grep "^${INPUT}=" "$BOOKMARKS_FILE")
-        grep -Fvx "$LINE" "$BOOKMARKS_FILE" > temp && mv temp "$BOOKMARKS_FILE"
+        grep -Fvx "$LINE" "$BOOKMARKS_FILE" >temp && mv temp "$BOOKMARKS_FILE"
         sed -i "1i $LINE" "$BOOKMARKS_FILE"
         $BROWSER "$LINK"
     fi
