8dd74156549b8c31bbd179ed35a32c79e89cc641
Novaturiente <Novaturiente@proton.me>
2025-10-03 13:02:22 +0530

    updated rofi scripts

--- scripts/rofi/power.sh
+++ scripts/rofi/power.sh
@@ -9,15 +9,15 @@ selected=$(printf '%s\n' "${options[@]}" | rofi -dmenu -p "󰮫 " -theme power)
 # Execute the selected action
 case "$selected" in
     "  Shutdown")
-        confirm=$(echo -e "Yes\nNo" | rofi -dmenu -p "Shutdown?" -theme power-menu)
+        confirm=$(echo -e "Yes\nNo" | rofi -dmenu -p "Shutdown?" -theme power)
 	[[ "$confirm" == "Yes" ]] && systemctl poweroff
         ;;
     "󰜉  Reboot")
-        confirm=$(echo -e "Yes\nNo" | rofi -dmenu -p "Reboot?" -theme power-menu)
+        confirm=$(echo -e "Yes\nNo" | rofi -dmenu -p "Reboot?" -theme power)
         [[ "$confirm" == "Yes" ]] && systemctl reboot
         ;;
     "󰍃  Logout")
-        confirm=$(echo -e "Yes\nNo" | rofi -dmenu -p "Logout?" -theme power-menu)
+        confirm=$(echo -e "Yes\nNo" | rofi -dmenu -p "Logout?" -theme power)
 	[[ "$confirm" == "Yes" ]] && loginctl terminate-user "$USER"
         ;;
  
